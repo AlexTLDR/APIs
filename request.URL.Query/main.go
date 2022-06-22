d1486c8da7a6b7fb7239c3550cee518f845d1260
Alex <abadragan@gmail.com>
2022-06-22 12:33:31 +0200

    port randomization

--- request.URL.Query/main.go
+++ request.URL.Query/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is the root path. For the hello, please go to /hello and add the name in the form ?name="))
+		//w.Write([]byte("This is the root path. For the hello, please go to /hello and add the name in the form ?name="))
+		fmt.Fprint(w, "This is the root path. For the hello, please go to /hello and add the name in the form ?name=")
 	})
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		helloFunc(w, r)
 	})
-	log.Println("The port on which the service is running is 8081")
-	http.ListenAndServe(":8081", nil)
+	//http.ListenAndServe(":8081", nil)
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+
+	panic(http.Serve(listener, nil))
 
 }
 
@@ -24,5 +35,6 @@ func helloFunc(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "world"
 	}
-	w.Write([]byte("Hello " + name))
+	//w.Write([]byte("Hello " + name))
+	fmt.Fprint(w, "Hello "+name)
 }
--- requestForm/main.go
+++ requestForm/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//handlerFunc(w, r)
-		w.Write([]byte("This is the root path. For the hello, please go to /hello and add the name in the form ?name="))
+		//w.Write([]byte("This is the root path. For the hello, please go to /hello and add the name in the form ?name="))
+		fmt.Fprint(w, "This is the root path. For the hello, please go to /hello and add the name in the form ?name=")
 	})
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		helloFunc(w, r)
 	})
-	log.Println("The port on which the service is running is 8081")
-	http.ListenAndServe(":8081", nil)
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+
+	panic(http.Serve(listener, nil))
+
+	//http.ListenAndServe(":0", nil) - commented out as it is not used anymore
 
 }
-
-//Isn't it simpler to use fmt.Fprint instead of w.Write([]byte("my string"))?
-
-/*func handlerFunc(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "This is the root path. For the hello, please go to /hello")
-	} else if r.URL.Path == "/hello" {
-		fmt.Fprint(w, "Hello world!")
-	}
-}*/
 
 func helloFunc(w http.ResponseWriter, r *http.Request) {
 
@@ -36,5 +36,6 @@ func helloFunc(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("name") == "" {
 		name = "world"
 	}
-	w.Write([]byte("Hello " + name))
+	//w.Write([]byte("Hello " + name))
+	fmt.Fprint(w, "Hello "+name)
 }
