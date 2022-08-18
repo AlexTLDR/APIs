eec8e028cb845ac2a6331e5d211100cf34c76958
Alex <abadragan@gmail.com>
2022-08-18 20:41:09 +0200

    simple API file uploader for google cloud

---
+++ googleCloudUploader/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	projectID  = "aiggato-upload"
+	bucketName = "aiggato-files"
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+var uploader *ClientUploader
+
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/home/alex/Keys/GoogleCloud/aiggato/aiggato-upload-18942db9665f.json") // the path to the connection json
+	client, err := storage.NewClient(context.Background())
+	fmt.Println(client)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "upload-files/", // the path of the files in the bucket
+	}
+
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/upload", func(c *gin.Context) {
+		f, err := c.FormFile("file-input") //the name of the form
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		blobFile, err := f.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		err = uploader.UploadFile(blobFile, f.Filename)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run() // listen and serve "localhost:8080"
+}
+
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
