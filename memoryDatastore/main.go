8553f35db4a6b46a6ad641061c3d10a2fc25190d
Alex <abadragan@gmail.com>
2022-08-31 17:31:46 +0200

    added the gcp uploader

--- memoryDatastore/main.go
+++ memoryDatastore/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"cloud.google.com/go/storage"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -15,12 +19,24 @@ type Contact struct {
 	Mail      string `json:"mail"`
 }
 
+const (
+	projectID  = "aiggato-upload"
+	bucketName = "aiggato-files"
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
 type server struct {
 	db *sql.DB
 }
 
 func main() {
-
+	/*sql start*/
 	db, err := sql.Open("mysql", "root:123456@tcp(127.0.01:3306)/aiggato")
 	if err != nil {
 		panic(err)
@@ -41,6 +57,56 @@ func main() {
 	}
 	r := router(s)
 
-	log.Fatal(http.ListenAndServe(":8081", r))
+	//log.Fatal(http.ListenAndServe(":8081", r))
+	go http.ListenAndServe(":8081", r)
+	/*sql end*/
+
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/home/alex/Keys/GoogleCloud/aiggato/aiggato-upload-18942db9665f.json") // the path to the connection json
+
+	client, err := storage.NewClient(context.Background())
+	fmt.Println(client)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	uploader := &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "upload-files/", // the path of the files in the bucket
+	}
+
+	g := gin.Default() // create a function
+	g.POST("/upload", func(c *gin.Context) {
+		f, err := c.FormFile("file-input") //the name of the form
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		blobFile, err := f.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		err = uploader.UploadFile(blobFile, f.Filename)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	g.Run(":8082") // listen and serve "localhost:8080"
 
 }
---
+++ memoryDatastore/upload_to_gcp.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"time"
+)
+
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
